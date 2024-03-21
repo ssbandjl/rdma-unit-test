73dcae54d644e0f59a9bb4f0cce6dcc78f8e2630
root <root@bin1.chinachina.cn>
2024-03-21 09:06:28 +0800

    s

---
+++ build.sh
@@ -0,0 +1,3 @@
+bazel test ... --test_output=all --test_arg=--verbs_mtu=1024 --test_arg=--ipv4_only --keep_going --nobuild
+
+
---
+++ docker.sh
@@ -0,0 +1,2 @@
+docker exec -u root -it rdma_ut bash -c 'cd /root/project/rdma/rdma-unit-test;exec "${SHELL:-sh}"'
+
